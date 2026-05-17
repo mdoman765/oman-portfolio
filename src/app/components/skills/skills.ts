9b04824670b8b6f4c8ed552c7944bd3a25b4ba84
Oman <mdoman765@gmail.com>
2026-05-17 17:36:51 +0600

    refactor skills.ts

--- src/app/components/skills/skills.ts
+++ src/app/components/skills/skills.ts
@@ -12,44 +12,45 @@ export class Skills {
       name: 'Programming Languages',
       icon: '💻',
       skills: [
-        { name: 'C#',         level: 92 },
-        { name: 'C / C++',    level: 88 },
-        { name: 'JavaScript', level: 85 },
-        { name: 'Python',     level: 82 },
-        { name: 'Linux/Bash', level: 70 },
+        { name: 'C / C++',         level: 95 },
+        { name: 'C#',    level: 80 },
+        { name: 'JavaScript', level: 65 },
+        { name: 'Python',     level: 60 },
+        { name: 'TypeScript', level: 60 },
+       
       ]
     },
     {
       name: 'Frameworks & Web',
       icon: '🌐',
       skills: [
-        { name: 'ASP.NET Core / Web API', level: 93 },
-        { name: 'Angular',                level: 85 },
-        { name: 'Entity Framework Core',  level: 88 },
-        { name: 'MVC Pattern',            level: 90 },
-        { name: 'Bootstrap / jQuery',     level: 80 },
+        { name: 'ASP.NET Core / Web API', level: 85 },
+         { name: 'MVC Pattern,N-tier Architecture',            level: 85},
+        { name: 'Angular',                level: 60 },
+        { name: 'Ajax',     level: 70 },
       ]
     },
     {
       name: 'AI & Automation',
       icon: '🤖',
       skills: [
-        { name: 'LLM Integration',        level: 88 },
-        { name: 'AI Agent Development',   level: 85 },
-        { name: 'n8n Workflow Automation',level: 80 },
+        { name: 'LLM Integration',        level: 95 },
+        { name: 'AI Agent Development',   level: 95 },
+        { name: 'n8n Workflow Automation',level: 95 },
         { name: 'NLP / Transformers',     level: 75 },
-        { name: 'Signal Processing',      level: 72 },
+        { name: 'Signal Processing',      level: 75 },
       ]
     },
     {
       name: 'Databases & DevOps',
       icon: '☁️',
       skills: [
-        { name: 'MSSQL / T-SQL',  level: 88 },
-        { name: 'MongoDB',        level: 78 },
-        { name: 'Docker',         level: 75 },
-        { name: 'IIS Deployment', level: 82 },
-        { name: 'Git / GitHub',   level: 90 },
+        { name: 'MSSQL ',  level: 95 },
+          { name: 'Git / GitHub,Ci/CD',   level: 95 },
+        { name: 'IIS Deployment', level: 95 },
+        { name: 'MongoDB',        level: 70 },
+        { name: 'Docker',         level: 65 },
+    
       ]
     }
   ];
